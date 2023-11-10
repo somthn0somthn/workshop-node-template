aac0904708bd1e91dde7fb93594f78cdc908d1f2
somthn0somthn <lpmaho@gmail.com>
2023-11-11 01:47:09 +0900

    workshop 04

--- pallets/collectibles/src/lib.rs
+++ pallets/collectibles/src/lib.rs
@@ -4,14 +4,41 @@ pub use pallet::*;
 
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
-    use frame_support::pallet_prelude::*;
-    use frame_system::pallet_prelude::*;
+	use frame_support::{
+		pallet_prelude::*,
+		traits::{Currency, Randomness},
+	};
+	use frame_system::pallet_prelude::*;
 
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    pub struct Pallet<T>(_);
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub enum Color {
+		Red,
+		Yellow,
+		Blue,
+		Green,
+	}
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-    }
+	type BalanceOf<T> =
+		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub struct Collectible<T: Config> {
+		pub unique_id: [u8; 16],
+		pub price: Option<BalanceOf<T>>,
+		pub color: Color,
+		pub owner: T::AccountId,
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Currency: Currency<Self::AccountId>;
+		type CollectionRandomness: Randomness<Self::Hash, BlockNumberFor<Self>>;
+
+		#[pallet::constant]
+		type MaximumOwned: Get<u32>;
+	}
 }
